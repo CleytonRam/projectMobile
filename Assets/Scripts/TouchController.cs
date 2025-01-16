ad5cc896f1f6ae535dd6ec01a9db63e43bb28917
CleytonRam <cleytonglauco@gmail.com>
2025-01-16 12:44:45 -0300

    feat: added touch controller and lerp helper

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class TouchController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector2 pastPosition;
+    public float velocity = 1f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
-        
+        if (Input.GetMouseButton(0)) 
+        {
+            //mousePosition AGORA - mousePosition passado
+            Move(Input.mousePosition.x - pastPosition.x);
+        }
+        pastPosition = Input.mousePosition;
+    }
+
+
+    public void Move(float speed)
+    {
+        transform.position += Vector3.right * Time.deltaTime * speed * velocity;
     }
 }
